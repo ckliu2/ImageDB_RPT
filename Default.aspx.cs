b5ce39b6b40463f9e8c6a3b396b787b8737fbeb2
chongkai <chongkai@livemail.tw>
2020-04-12 17:24:33 +0800

    0412

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -352,8 +352,20 @@ public partial class _Default : System.Web.UI.Page
                     report.SetParameterValue("caseClosed", Request["caseClosed"] );
                     report.SetParameterValue("customerName", Request["customerName"] );
                     report.SetParameterValue("salemanName", Request["salemanName"] );
-                    
                     saveDisk1(report, randomId);               
+                break;
+                
+                case 41:
+                    rptFile = this.Server.MapPath("rpt/quotedPriceCustomerList1.rpt");
+                    report.Load(rptFile);                    
+                    report.SetDatabaseLogon(UserID, UserPassword, DBIP, "ImageDB");
+                    report.SetParameterValue("startDate", Request["startDate"] );
+                    report.SetParameterValue("endDate", Request["endDate"] );
+                    report.SetParameterValue("produce", Request["produce"] );
+                    report.SetParameterValue("caseClosed", Request["caseClosed"] );
+                    report.SetParameterValue("customerName", Request["customerName"] );
+                    report.SetParameterValue("salemanName", Request["salemanName"] );
+                    saveDisk1(report, randomId);    
                 break;
                 
                 
